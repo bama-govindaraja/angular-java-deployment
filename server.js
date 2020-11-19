53bd67d89b0fd5d07352f3d12984935327d0e21a
Bama Govindaraja <bama.govindara@pb.com>
2020-11-19 00:06:24 -0500

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //Install express server
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
-
+app.use(cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-routingdemo'));
 
@@ -13,4 +14,4 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
