40226a21350088a860fe074ad9324ed00b979928
Bama Govindaraja <bama.govindara@pb.com>
2020-11-18 23:58:26 -0500

    heroku deployment

--- src/services/CourseService.ts
+++ src/services/CourseService.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseService {
 findAllCourses = () =>
-fetch('https://CoursedemoDev-env.eba-kkep726v.us-east-1.elasticbeanstalk.com/api/courses')
+fetch('http://CoursedemoDev-env.eba-kkep726v.us-east-1.elasticbeanstalk.com/api/courses')
   .then(response => response.json())
 }
